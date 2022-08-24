ad1a8d25b192869ab8612f07502cebcbda601366
Richard Strickler <richastrick1998@gmail.com>
2022-08-24 13:58:48 -0500

    SQLTest First

--- src/main/java/com/training/assignments/sqlTest/App.java
+++ src/main/java/com/training/assignments/sqlTest/App.java
@@ -1,13 +1,36 @@
 package com.training.assignments.sqlTest;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws ClassNotFoundException, SQLException
     {
-        System.out.println( "Hello World!" );
+	    Connection connection = null;
+	    try {
+	    	connection = DriverManager
+	    			.getConnection("jdbc:mysql://localhost:3306/JDBCDemo", "root", "S2xj8!efD4m37");
+	    	
+	    	if(connection.isValid(0)) System.out.println("Connection Successful!");
+	    	
+	    } catch (SQLException e) {
+	        System.out.println("Connection Failed!");
+	        return;
+	    } finally {
+	        try
+	        {
+	          if(connection != null)
+	            connection.close();
+	          System.out.println("Connection closed !!");
+	        } catch (SQLException e) {
+	          e.printStackTrace();
+	        }
+	    }
     }
 }
